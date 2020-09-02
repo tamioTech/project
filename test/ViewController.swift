974b7c63ac85dcdad8614b254adcbb8ab7f273c0
tamio <tyler.hikida@gmail.com>
2020-09-01 20:21:26 -0700

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Tyler Hikida on 2020-09-01.
+//  Copyright © 2020 tamioTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
